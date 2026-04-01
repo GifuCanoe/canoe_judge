cf17aa85269bcc3b3c2980d2d11fe741d7f55de6
GifuCanoe <sa10shi.ka10@gmail.com>
2026-04-01 21:37:01 +0900

    Add files via upload

--- WW/config.js
+++ WW/config.js
@@ -1,4 +1,4 @@
 // 全システム共通の設定
 const APP_CONFIG = {
-    GAS_URL: "https://script.google.com/macros/s/AKfycbxwTmBwAfQmp6QnTAo526AR-PlZ-WVfzQsF3Khyldmo5FXBSjEPuXgwmB1SkIJnNYPk/exec"
+    GAS_URL: "https://script.google.com/macros/s/AKfycbxHQOG89fjy0F9WcHNlGZeyx5_0E0J0ByX9sBKvN5i9FEX4BY9yzy27OwsRBHjVk2HP/exec"
 };
