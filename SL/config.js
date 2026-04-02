2e654760e2fe00e224accec2bdd79cd4d1499951
GifuCanoe <sa10shi.ka10@gmail.com>
2026-04-02 21:51:38 +0900

    Add files via upload

--- SL/config.js
+++ SL/config.js
@@ -4,10 +4,10 @@ const APP_CONFIG = {
 
     // 区間ごとのゲート設定（ここを編集するだけでOK）
     TOURNAMENT_CONFIG: {
-        1: { startGate: 1,  numGates: 3 },
-        2: { startGate: 4,  numGates: 3 },
-        3: { startGate: 7,  numGates: 3 },
-        4: { startGate: 10, numGates: 3 },
-        5: { startGate: 13, numGates: 3 }
+        1: { startGate: 1,  numGates: 4 },
+        2: { startGate: 5,  numGates: 4 },
+        3: { startGate: 9,  numGates: 4 },
+        4: { startGate: 13, numGates: 3 },
+        5: { startGate: 16, numGates: 2 }
     }
 };
