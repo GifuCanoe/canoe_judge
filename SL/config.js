599c4adcd250505088c429d1c8c364ba129d1c2a
GifuCanoe <sa10shi.ka10@gmail.com>
2026-05-02 10:16:11 +0900

    Add files via upload

--- SL/config.js
+++ SL/config.js
@@ -1,13 +1,13 @@
-// 全システム共通の設定
+// 全システム共通の設定_SL用
 const APP_CONFIG = {
-    GAS_URL: "https://script.google.com/macros/s/AKfycbwKfM-Xrt4z7RHnHjRarxj724DUz9r3QQ-mbAV-CxnYQjxqm-uc3CcqXEw4AW_D27j1ZQ/exec",
+    GAS_URL: "https://script.google.com/macros/s/AKfycbx3zNaU-DZSc18lndI5InCtzYFqyrx19HR-LWYdh0VKFKlBfI9Tr2plw6Niro-l83zUDA/exec",
 
     // 区間ごとのゲート設定（ここを編集するだけでOK）
     TOURNAMENT_CONFIG: {
-        1: { startGate: 1,  numGates: 4 },
-        2: { startGate: 5,  numGates: 4 },
-        3: { startGate: 9,  numGates: 4 },
-        4: { startGate: 13, numGates: 3 },
-        5: { startGate: 16, numGates: 2 }
+        1: { startGate: 1,  numGates: 3 },
+        2: { startGate: 4,  numGates: 3 },
+        3: { startGate: 7,  numGates: 3 },
+        4: { startGate: 10, numGates: 3 },
+        5: { startGate: 13, numGates: 3 }
     }
 };
